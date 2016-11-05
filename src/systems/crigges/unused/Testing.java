34913d44e1f8d0cfa44fa9a8d3eb56c6898240f4
unknown <andy400@web.de>
2016-11-05 15:05:29 +0100

    General code improovements | Basic functions working flawless now

---
+++ src/systems/crigges/unused/Testing.java
@@ -0,0 +1,11 @@
+package systems.crigges.unused;
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		for(int i = 0; i < 30; i++){
+			System.out.println(((i / 4) * 4) + (3 - (i % 4)));
+		}
+	}
+
+}
